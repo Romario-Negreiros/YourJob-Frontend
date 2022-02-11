8e5aedda378042d0761526b00cb767c5cd700070
Romario Negreiros <nromario482@gmail.com>
2022-02-10 22:32:34 -0300

    Created default folders to initialize the project + Set up the rendering flow (layouts, pages, routes etc)

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+const Pages: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" />
+    </Routes>
+  )
+}
+
+export default Pages
