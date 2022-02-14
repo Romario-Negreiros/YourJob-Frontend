80fd48cbbd3cd804550a5c153d912abe8ba48b67
Romario Negreiros <nromario482@gmail.com>
2022-02-14 00:18:03 -0300

    Created folder for first component: loader

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader: React.FC = () => {
+  return (
+    <div>
+      laoder loader lodarelaod oelaooadoadaodaodado
+    </div>
+  )
+}
+
+export default Loader
