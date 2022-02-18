558ef75551d2ead0ec9b7bbaf460165b29967fb1
Romario Negreiros <nromario482@gmail.com>
2022-02-17 23:55:33 -0300

    Created light theme + Created reset styles file + Created assets folder to hold images, animations etc.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import { ThemeProvider } from '@mui/material/styles'
 import { light } from './themes'
 import { BrowserRouter } from 'react-router-dom'
+import { ScrollToTop } from './components'
 import Pages from './pages'
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <ThemeProvider theme={light}>
+        <ScrollToTop />
         <Pages />
       </ThemeProvider>
     </BrowserRouter>
--- src/themes/light.ts
+++ src/themes/light.ts
@@ -1,20 +1,8 @@
 import { createTheme } from '@mui/material/styles'
-import amber from '@mui/material/colors/amber'
-import green from '@mui/material/colors/green'
 
 const light = createTheme({
   palette: {
-    mode: 'light',
-    primary: amber,
-    secondary: green,
-    background: {
-      default: '#fff4b0',
-      paper: '#00c853'
-    },
-    text: {
-      primary: '#121212',
-      secondary: '#3b38ff'
-    }
+    mode: 'light'
   }
 })
 
