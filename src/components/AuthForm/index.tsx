084468a7adaafd2c01876ae88c039aceaf006cac
Romario Negreiros <nromario482@gmail.com>
2022-02-20 14:02:27 -0300

    Created styles of AuthForm and UserProfileForm

--- src/components/AuthForm/index.tsx
+++ src/components/AuthForm/index.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 
+import useStyles from './styles'
+
+import Grid from '@mui/material/Grid'
+import TextField from '@mui/material/TextField'
+
 const AuthForm: React.FC = () => {
+  const classes = useStyles()
+
   return (
-    <>
-    Authentication informations form
-    </>
+    <Grid container className={classes.grid} rowSpacing={4}>
+      <Grid item xs={12} sm={6} sx={{ textAlign: 'center' }}>
+        <TextField label="Name" sx={{ width: 240 }} />
+      </Grid>
+      <Grid item xs={12} sm={6} sx={{ textAlign: 'center' }}>
+        <TextField label="Email" sx={{ width: 240 }} />
+      </Grid>
+      <Grid item xs={12} sm={6} sx={{ textAlign: 'center' }}>
+        <TextField label="Password" sx={{ width: 240 }} />
+      </Grid>
+      <Grid item xs={12} sm={6} sx={{ textAlign: 'center' }}>
+        <TextField label="Confirm Password" sx={{ width: 240 }} />
+      </Grid>
+    </Grid>
   )
 }
 
--- src/components/UserProfileForm/index.tsx
+++ src/components/UserProfileForm/index.tsx
@@ -1,26 +1,42 @@
 import React from 'react'
 
 import useStyles from './styles'
+import { styled } from '@mui/material/styles'
 
 import Grid from '@mui/material/Grid'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 import TextField from '@mui/material/TextField'
 
 const UserProfileForm: React.FC = () => {
   const classes = useStyles()
 
+  const Input = styled('input')({
+    display: 'none'
+  })
+
   return (
     <Grid container className={classes.grid} rowSpacing={4}>
-      <Grid item xs={12} sm={6} sx={{ textAlign: 'center' }}>
-        <TextField label="Name" sx={{ width: 240 }} />
+      <Grid item sx={{ textAlign: 'center' }} xs={12}>
+        <TextField label="Bio" sx={{ width: 240 }} />
       </Grid>
-      <Grid item xs={12} sm={6} sx={{ textAlign: 'center' }}>
-        <TextField label="Email" sx={{ width: 240 }} />
+      <Grid item sx={{ textAlign: 'center' }} xs={12} sm={6}>
+      <label htmlFor="profile-picture">
+        <Typography variant="body2">Profile picture</Typography>
+        <Input type="file" accept="image/*" id="profile-picture" sx={{ display: 'none' }}/>
+        <Button variant="contained" color="secondary" component="span">
+          Upload
+        </Button>
+      </label>
       </Grid>
-      <Grid item xs={12} sm={6} sx={{ textAlign: 'center' }}>
-        <TextField label="Password" sx={{ width: 240 }} />
-      </Grid>
-      <Grid item xs={12} sm={6} sx={{ textAlign: 'center' }}>
-        <TextField label="Confirm Password" sx={{ width: 240 }} />
+      <Grid item sx={{ textAlign: 'center' }} xs={12} sm={6}>
+        <label htmlFor="curriculum">
+        <Typography variant="body2">Curriculum</Typography>
+          <Input type="file" accept="file/*" id="curriculum" sx={{ display: 'none' }}/>
+          <Button variant="contained" color="secondary" component="span">
+            Upload
+          </Button>
+        </label>
       </Grid>
     </Grid>
   )
