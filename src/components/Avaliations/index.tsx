fb4f9c98858dc8832b0b56e45ffb990f9f2a52ab
Romario Negreiros <nromario482@gmail.com>
2022-02-24 11:29:13 -0300

    Created components do display a company's avaliations + Created component for creating avaliations of a company

---
+++ src/components/Avaliations/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import Grid from '@mui/material/Grid'
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
+import CardActions from '@mui/material/CardActions'
+import Typography from '@mui/material/Typography'
+import Avatar from '@mui/material/Avatar'
+import Rating from '@mui/material/Rating'
+
+const Avaliations: React.FC = () => {
+  return (
+    <Grid container spacing={2}>
+      {new Array(10).fill(1).map((v, i) => (
+        <Grid item key={i + 'ok'} xs={12} md={6} lg={4}>
+          <Card>
+            <CardHeader
+              avatar={<Avatar aria-label="vagancy">R</Avatar>}
+              title="user.name"
+              subheader="avaliation.createdAt"
+            />
+            <CardContent>
+              <Typography variant="body1" color="text.primary">
+                This impressive paella is a perfect party dish and a fun meal to cook together with
+                your guests. Add 1 cup of frozen peas along with the mussels, if you like.
+              </Typography>
+              <Typography variant="body2" component="div" sx={{ mt: 2 }}>Not recommended</Typography>
+            </CardContent>
+            <CardActions>
+              <Rating name="half-rating" defaultValue={2.5} precision={0.5} readOnly />
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  )
+}
+
+export default Avaliations
---
+++ src/components/CreateAvaliationForm/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+import Grid from '@mui/material/Grid'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select, { SelectChangeEvent } from '@mui/material/Select'
+
+const CreateAvaliationForm: React.FC = () => {
+  const [recommendation, setRecommendation] = React.useState('')
+  const [grade, setGrade] = React.useState<string | number>('')
+
+  const handleRecommendationChange = (event: SelectChangeEvent) =>
+    setRecommendation(event.target.value)
+  const handleGradeChange = (event: SelectChangeEvent) => setGrade(Number(event.target.value))
+
+  return (
+    <Grid container component="form" spacing={4}>
+      <Grid item xs={12} sx={{ textAlign: 'center' }}>
+        <TextField label="Comment" multiline rows={4} sx={{ width: '100%' }} />
+      </Grid>
+      <Grid item xs={12} md={6} sx={{ textAlign: 'center' }}>
+        <FormControl sx={{ width: 240 }}>
+          <InputLabel id="grade">Grade</InputLabel>
+          <Select labelId="grade" value={String(grade)} onChange={handleGradeChange} label="Grade">
+            <MenuItem value={0}>0</MenuItem>
+            <MenuItem value={1}>1</MenuItem>
+            <MenuItem value={2}>2</MenuItem>
+            <MenuItem value={3}>3</MenuItem>
+            <MenuItem value={4}>4</MenuItem>
+            <MenuItem value={5}>5</MenuItem>
+          </Select>
+        </FormControl>{' '}
+      </Grid>
+      <Grid item xs={12} md={6} sx={{ textAlign: 'center' }}>
+        <FormControl sx={{ width: 240 }}>
+          <InputLabel id="recommendation">Recommendation</InputLabel>
+          <Select
+            labelId="recommendation"
+            value={recommendation}
+            onChange={handleRecommendationChange}
+            label="Recommendation"
+          >
+            <MenuItem value={'I recommend'}>I recommend</MenuItem>
+            <MenuItem value={"I don't recommend"}>I don&apos;t recommend</MenuItem>
+            <MenuItem value={"I'm neutral"}>I&apos;m neutral</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} sx={{ textAlign: 'center' }}>
+        <Button variant="contained" type="submit">
+          Create
+        </Button>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default CreateAvaliationForm
