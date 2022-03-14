1970a7a39f40861b97e285a45a45dbdc7967cd55
Romario Negreiros <nromario482@gmail.com>
2022-03-14 11:20:49 -0300

    Reorganized lib folders

--- src/lib/firebase/index.ts
+++ src/lib/firebase/index.ts
@@ -2,10 +2,10 @@ import { initializeApp } from 'firebase/app'
 import { getStorage, ref, uploadBytesResumable, deleteObject, getDownloadURL } from 'firebase/storage'
 
 const firebaseConfig = {
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID
