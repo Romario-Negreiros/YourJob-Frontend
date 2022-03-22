baefb48d0910315cb729025551369d40f78f43a6
Romario Negreiros <nromario482@gmail.com>
2022-03-22 11:33:47 -0300

    Refactored header component + removed some icons + rendering profile and login/logout icons conditionally

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 import menuItems from './menuItems'
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
+import { clearUser } from '../../app/slices/user'
+import { clearCompany } from '../../app/slices/company'
 
 import { Link } from 'react-router-dom'
 import Box from '@mui/material/Box'
@@ -10,18 +13,33 @@ import Typography from '@mui/material/Typography'
 import IconButton from '@mui/material/IconButton'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
+import Tooltip from '@mui/material/Tooltip'
 
+import AccountIcon from '@mui/icons-material/AccountBox'
+import LoginRoundedIcon from '@mui/icons-material/LoginRounded'
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded'
 import MenuIcon from '@mui/icons-material/Menu'
 import CloseIcon from '@mui/icons-material/Close'
 
 const Header: React.FC = () => {
   const [menuAnchorEl, setMenuAnchorEl] = React.useState<null | HTMLElement>()
   const isMenuOpen = Boolean(menuAnchorEl)
+  const dispatch = useAppDispatch()
+  const currentState = useAppSelector(state => state)
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     if (window.innerWidth <= 600) setMenuAnchorEl(event.currentTarget)
   }
   const handleCloseMenu = () => setMenuAnchorEl(null)
+
+  const handleLogout = () => {
+    if (currentState.user.data) {
+      dispatch(clearUser())
+    } else if (currentState.company.data) {
+      dispatch(clearCompany())
+    }
+    localStorage.removeItem('jwt')
+  }
 
   return (
     <AppBar
@@ -45,12 +63,49 @@ const Header: React.FC = () => {
         </Box>
         <Box sx={{ display: { xs: 'none', sm: 'flex' } }}>
           {menuItems.map(item => (
-            <Link key={item.text} to={item.url} aria-label={item.ariaLabel}>
-              <IconButton size="large" edge="end">
-                {item.icon}
-              </IconButton>
-            </Link>
+            <Tooltip key={item.text} title={item.text} placement={item.text === 'Home' ? 'bottom-start' : 'bottom'}>
+              <Link to={item.url} aria-label={item.ariaLabel}>
+                <IconButton size="large" edge="end">
+                  {item.icon}
+                </IconButton>
+              </Link>
+            </Tooltip>
           ))}
+          {currentState.user.data && (
+            <Tooltip title="Profile" placement="bottom-start">
+              <Link to="/profile" aria-label="Go to profile page">
+                <IconButton size="large" edge="end">
+                  <AccountIcon />
+                </IconButton>
+              </Link>
+            </Tooltip>
+          )}
+          {currentState.company.data && (
+            <Tooltip title="Profile" placement="bottom-start">
+              <Link to="/companies/profile" aria-label="Go to profile page">
+                <IconButton size="large" edge="end">
+                  <AccountIcon />
+                </IconButton>
+              </Link>
+            </Tooltip>
+          )}
+          {currentState.user.data || currentState.company.data
+            ? (
+            <Tooltip title="Log out" placement="bottom-start">
+              <IconButton size="large" edge="end" onClick={handleLogout}>
+                <LogoutRoundedIcon />
+              </IconButton>
+            </Tooltip>
+              )
+            : (
+            <Tooltip title="Log in" placement="bottom-start">
+              <Link to="/login" aria-label="Go to login page">
+                <IconButton size="large" edge="end">
+                  <LoginRoundedIcon />
+                </IconButton>
+              </Link>
+            </Tooltip>
+              )}
         </Box>
         <Menu
           id="mobile-menu"
@@ -72,6 +127,33 @@ const Header: React.FC = () => {
               <MenuItem>{item.text}</MenuItem>
             </Link>
           ))}
+          {currentState.user.data && (
+            <Link to="/profile" aria-label="Go to profile page" onClick={handleCloseMenu}>
+              <MenuItem>Profile</MenuItem>
+            </Link>
+          )}
+          {currentState.company.data && (
+            <Link to="/companies/profile" aria-label="Go to profile page" onClick={handleCloseMenu}>
+              <MenuItem>Profile</MenuItem>
+            </Link>
+          )}
+          {currentState.user.data || currentState.company.data
+            ? (
+            <MenuItem
+              aria-label="Log out"
+              onClick={() => {
+                handleLogout()
+                handleCloseMenu()
+              }}
+            >
+              Log out
+            </MenuItem>
+              )
+            : (
+            <Link to="/login" aria-label="Go to login page" onClick={handleCloseMenu}>
+              <MenuItem>Log in</MenuItem>
+            </Link>
+              )}
         </Menu>
       </Toolbar>
     </AppBar>
--- src/layouts/Header/menuItems.tsx
+++ src/layouts/Header/menuItems.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import { MenuItem } from './interfaces'
 
-import AccountIcon from '@mui/icons-material/AccountBox'
 import BusinessIcon from '@mui/icons-material/Business'
 import HomeIcon from '@mui/icons-material/Home'
-import HomeWorkIcon from '@mui/icons-material/HomeWork'
-import GroupIcon from '@mui/icons-material/Group'
-import LoginRoundedIcon from '@mui/icons-material/LoginRounded'
-// import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded'
 
 const menuItems: MenuItem[] = [
   {
@@ -17,34 +12,10 @@ const menuItems: MenuItem[] = [
     icon: <HomeIcon />
   },
   {
-    url: '/users',
-    ariaLabel: 'Go to users list page',
-    text: 'Users list',
-    icon: <GroupIcon />
-  },
-  {
-    url: '/companies/list',
-    ariaLabel: 'Go to companies list page',
-    text: 'Companies list',
-    icon: <HomeWorkIcon />
-  },
-  {
     url: '/companies',
     ariaLabel: 'Go to companies home page',
     text: 'For companies',
     icon: <BusinessIcon />
-  },
-  {
-    url: '/profile',
-    ariaLabel: 'Go to profile page',
-    text: 'Profile',
-    icon: <AccountIcon />
-  },
-  {
-    url: '/login',
-    ariaLabel: 'Go to log in page',
-    text: 'Log in',
-    icon: <LoginRoundedIcon />
   }
 ]
 
