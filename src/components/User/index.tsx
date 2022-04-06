cd0373591a8d4846eeae7f1d781c7bc70c9b0937
Romario Negreiros <nromario482@gmail.com>
2022-04-06 14:41:03 -0300

    Added user information in user component + Added link to user profile in account icon of user component

--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -8,32 +8,38 @@ import CardActions from '@mui/material/CardActions'
 import Typography from '@mui/material/Typography'
 import Avatar from '@mui/material/Avatar'
 import IconButton from '@mui/material/IconButton'
+import { Link } from 'react-router-dom'
 
 import { Props } from './interfaces'
 
 import AccountIcon from '@mui/icons-material/AccountBox'
 
-const User: React.FC<Props> = ({ breakpoints }) => {
+const User: React.FC<Props> = ({ breakpoints, user }) => {
   const { xs, sm, md, lg } = breakpoints
 
   return (
     <Grid item xs={xs} sm={sm} md={md} lg={lg}>
       <Card>
         <CardHeader
-          avatar={<Avatar aria-label="vacancy">U</Avatar>}
-          title="user.name"
-          subheader="vacancy.createdAt"
+          avatar={
+            <Avatar aria-label="vacancy" src={user.profilePicture || ''}>
+              {user.name.charAt(0).toUpperCase()}
+            </Avatar>
+          }
+          title={user.name}
+          subheader={user.age + ' years old'}
         />
         <CardContent>
           <Typography variant="body2" color="text.primary">
-            This impressive paella is a perfect party dish and a fun meal to cook together with your
-            guests. Add 1 cup of frozen peas along with the mussels, if you like.
+            {user.bio}
           </Typography>
         </CardContent>
         <CardActions>
-          <IconButton size="medium" edge="end">
-            <AccountIcon />
-          </IconButton>
+          <Link to={`/profile/${user.id}`}>
+            <IconButton size="medium" edge="end">
+              <AccountIcon />
+            </IconButton>
+          </Link>
         </CardActions>
       </Card>
     </Grid>
--- src/components/User/interfaces.ts
+++ src/components/User/interfaces.ts
@@ -1,8 +1,11 @@
+import { User } from '../../app/slices/user/interfaces'
+
 export interface Props {
   breakpoints: {
-    xs: number;
-    sm: number;
-    md: number;
-    lg?: number;
+    xs: number
+    sm: number
+    md: number
+    lg?: number
   }
+  user: User
 }
